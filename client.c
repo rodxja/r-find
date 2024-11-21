daa4899d2c41191a3f66d50ccaac9cea7ea00885
rodxja <rodxja@gmail.com>
2024-11-20 19:23:02 -0600

    merge server  - client

--- client.c
+++ client.c
@@ -8,10 +8,111 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <stdlib.h>
+
 #define PORT 8080
 #define BUFFER_SIZE 1024
 #define SERVER_IP "localhost"
 
+void receive_file(int client_socket);
+char *getInput();
+void getPaths(const char *dir, const char *regex);
+void getFiles(const char *dir, const char *regex);
+void create_socket(const char *method, const char *uri);
+
+// rfind . -name SEARCH_NAME
+// rfind ./source -get -name "*.cpp"
+int main(int argc, char const *argv[])
+{
+    // extract values from  argv
+    if (argc != 4 && argc != 5)
+    {
+        printf("Usage: %s <dir> -name <regex>\n", argv[0]);
+        printf("Usage: %s <dir> -get -name <regex>\n", argv[0]);
+        return 1;
+    }
+
+    if (argc == 4)
+    {
+        // validate that the second argument is -name
+        if (strcmp(argv[2], "-name") != 0)
+        {
+            printf("Usage: %s <dir> -name <regex>\n", argv[0]);
+            return 1;
+        }
+        getPaths(argv[1], argv[3]);
+    }
+    else
+    {
+        // validate that the second argument is -get
+        if (strcmp(argv[2], "-get") != 0)
+        {
+            printf("Usage: %s <dir> -get -name <regex>\n", argv[0]);
+            return 1;
+        }
+
+        // validate that the fourth argument is -name
+        if (strcmp(argv[3], "-name") != 0)
+        {
+            printf("Usage: %s <dir> -get -name <regex>\n", argv[0]);
+            return 1;
+        }
+
+        getFiles(argv[1], argv[4]);
+    }
+
+    return 0;
+}
+
+/*
+uri: /paths?dir=./source&regex=.*
+    /files?dir=./source&regex=.*
+ */
+void create_socket(const char *method, const char *uri)
+{
+    int status, client_fd;
+    struct sockaddr_in serv_addr;
+    char request[BUFFER_SIZE * 3];
+
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary
+    // form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf(
+            "\nInvalid address/ Address not supported \n");
+        return;
+    }
+
+    if ((status = connect(client_fd, (struct sockaddr *)&serv_addr,
+                          sizeof(serv_addr))) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return;
+    }
+
+    // Format the HTTP GET/POST/PUT/DELETE request with URI and query parameters
+    // ONLY SUPPORTS GET
+    snprintf(request, sizeof(request), "%s %s HTTP/1.0\r\nHost: %s\r\n\r\n", method, uri, SERVER_IP);
+
+    printf("Requesting file: %s\n", request);
+    send(client_fd, request, strlen(request), 0);
+
+    receive_file(client_fd);
+
+    // closing the connected socket
+    close(client_fd);
+}
+
+// TODO : this handle the responses from the server
 void receive_file(int client_socket)
 {
     char buffer[BUFFER_SIZE];
@@ -31,64 +132,47 @@ void receive_file(int client_socket)
     }
 }
 
-int main(int argc, char const *argv[])
+char *getInput()
 {
-    int status, client_fd;
-    struct sockaddr_in serv_addr;
-    char request[BUFFER_SIZE * 3];
-
-    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary
-    // form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-    {
-        printf(
-            "\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-
-    if ((status = connect(client_fd, (struct sockaddr *)&serv_addr,
-                          sizeof(serv_addr))) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-
-    printf("Enter filename: \n");
-    char filename[BUFFER_SIZE];
-    fgets(filename, BUFFER_SIZE, stdin);
+    char *input = malloc(BUFFER_SIZE); // TODO : use correct size
+    fgets(input, BUFFER_SIZE, stdin);
 
     // Remove the newline character if it exists
-    size_t len = strlen(filename);
-    if (len > 0 && filename[len - 1] == '\n')
+    size_t len = strlen(input);
+    if (len > 0 && input[len - 1] == '\n')
     {
-        filename[len - 1] = '\0'; // Replace newline with null terminator
+        input[len - 1] = '\0'; // Replace newline with null terminator
     }
 
-    const char *uri = "/file";   // Change to desired endpoint
-    char query[BUFFER_SIZE * 2]; // Your query parameters here
-
-    snprintf(query, sizeof(query), "filename=%s", filename);
-
-    // Format the HTTP GET request with URI and query parameters
-    snprintf(request, sizeof(request), "GET %s?%s HTTP/1.0\r\nHost: %s\r\n\r\n", uri, query, SERVER_IP);
-
-    printf("Requesting file: %s\n", request);
-    send(client_fd, request, strlen(request), 0);
-
-    receive_file(client_fd);
-
-    // closing the connected socket
-    close(client_fd);
-    return 0;
+    return input;
 }
 
-// gcc tarea5_4-2019243821_client.c -o tarea5_4-2019243821_client
+void getPaths(const char *dir, const char *regex)
+{
+    printf("dir: '%s'\n", dir);
+    printf("regex: '%s'\n", regex);
+
+    // TODO : use correct size
+    char *uri = malloc(BUFFER_SIZE);
+    snprintf(uri, BUFFER_SIZE, "/paths?dir=%s&regex=%s", dir, regex);
+
+    printf("uri: '%s'\n", uri);
+    create_socket("GET", uri);
+    // this will print the file to stdout
+}
+
+void getFiles(const char *dir, const char *regex)
+{
+    printf("dir: '%s'\n", dir);
+    printf("regex: '%s'\n", regex);
+
+    // TODO : use correct size
+    char *uri = malloc(BUFFER_SIZE);
+    snprintf(uri, BUFFER_SIZE, "/files?dir=%s&regex=%s", dir, regex);
+
+    printf("uri: '%s'\n", uri);
+    create_socket("GET", uri);
+    // this will create a file with the contents of the file
+}
+
+// gcc client.c -o rfind
--- server.c
+++ server.c
@@ -11,6 +11,16 @@
 
 // thread
 #include <pthread.h>
+#include <dirent.h>
+
+// regex
+#include <regex.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
@@ -19,153 +29,12 @@
 int clients[MAX_CLIENTS];
 int numClients = 0;
 
-void send_file(int client_socket, char *filename)
-{
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
-    {
-        perror("error opening file");
-        return;
-    }
-
-    // HTTP response with body content
-
-    char response[BUFFER_SIZE];
-
-    size_t bytes_read;
-    int i = 0;
-
-    const char *header = "HTTP/1.0 200 OK\r\n"
-                         "Server: webserver-c\r\n"
-                         "Content-Type: text/plain\r\n"
-                         "Content-Length: %ld\r\n\r\n";
-
-    int body_size = BUFFER_SIZE - strlen(header) - 1;
-    char body_content[body_size];
-
-    while ((bytes_read = fread(body_content, 1, body_size, file)) > 0)
-    {
-
-       
-        snprintf(response, sizeof(response),
-                 "HTTP/1.0 200 OK\r\n"
-                 "Server: webserver-c\r\n"
-                 "Content-Type: text/plain\r\n"
-                 "Content-Length: %ld\r\n\r\n" // Include Content-Length
-                 "%s",                         // Body content
-                 strlen(body_content), body_content);
-
-        printf("response: \n----------------\n%s\n----------------\n", response);
-
-        send(client_socket, response, BUFFER_SIZE, 0);
-
-        memset(body_content, 0, sizeof(body_content));
-        memset(response, 0, sizeof(response));
-
-        printf("\nsent iteration number '%d'\n", i++);
-    }
-
-    fclose(file);
-}
-
-void extract_filename(const char *uri, char *filename)
-{
-    // Look for "filename=" in the URI
-    char *start = strstr(uri, "filename=");
-    if (start)
-    {
-        // Move pointer to the beginning of the filename value
-        start += strlen("filename=");
-
-        // Copy the filename value until the end of the string or until '&' (if there are multiple parameters)
-        int i = 0;
-        while (start[i] != '\0' && start[i] != '&' && i < BUFFER_SIZE - 1)
-        {
-            filename[i] = start[i];
-            i++;
-        }
-        filename[i] = '\0'; // Null-terminate the filename string
-    }
-    else
-    {
-        // If "filename=" is not found, return an empty string
-        filename[0] = '\0';
-    }
-}
-
-
-void *handle_client(void *arg)
-{
-    int newsockfd = *((int *)arg);
-
-    // Get client address
-    int sockn = getsockname(newsockfd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addrlen);
-    if (sockn < 0)
-    {
-        perror("webserver (getsockname)");
-        return NULL;
-    }
-
-    char buffer[1024] = {0};
-    ssize_t valread;
-
-    // TODO : check difference between read and recv
-
-    // Read from the socket
-    int valread = recv(newsockfd, buffer, BUFFER_SIZE, 0);
-    if (valread < 0)
-    {
-        perror("webserver (read)");
-        continue;
-    }
-
-    printf("buffer: %s\n", buffer);
-
-    // TODO : create router func
-
-    // Read the request
-    char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
-    sscanf(buffer, "%s %s %s", method, uri, version);
-    printf("[%s:%u] method : '%s' version : '%s' uri : '%s'\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), method, version, uri);
-
-    char *filename = malloc(BUFFER_SIZE);
-    extract_filename(uri, filename);
-
-    send_file(newsockfd, filename);
-
-    close(newsockfd);
-    // TO THIS  MOVE TO handle_client/////////
-
-    /* while ((valread = read(client_socket, buffer, 1024 - 1)) > 0)
-    {
-        printf("got from client : '%s'\n", buffer);
-
-        broadcast(buffer, client_socket);
-
-        char *msg = "your message was broadcasted";
-
-        send(client_socket, msg, strlen(msg), 0);
-        printf("send to client : '%s'\n", msg);
-    }
-
-    // closing the connected socket
-    close(client_socket);
-    // remove from client list
-    for (int i = 0; i < numClients; i++)
-    {
-        if (clients[i] == client_socket)
-        {
-            for (int j = i; j < numClients - 1; j++)
-            {
-                clients[j] = clients[j + 1];
-            }
-            numClients--;
-            break;
-        }
-    } */
-
-    return NULL;
-}
+void searchFiles(const char *dir, const char *regex, int clientSocket);
+void send_file(int client_socket, char *filename);
+char *extract_path_parameter(const char *uri, char *parameterName);
+void paths(int client_socket, const char *uri);
+void files(int client_socket, const char *uri);
+void *handle_client(void *arg);
 
 int main()
 {
@@ -221,8 +90,8 @@ int main()
 
         if (numClients == MAX_CLIENTS)
         {
-           printf("server cannot accept more clients\n"); 
-           close(newsockfd);
+            printf("server cannot accept more clients\n");
+            close(newsockfd);
         }
 
         clients[numClients++] = newsockfd;
@@ -238,7 +107,250 @@ int main()
     return 0;
 }
 
-// gcc http_server.c -o http_server
+// close in each return
+void *handle_client(void *arg)
+{
+    int newsockfd = *((int *)arg);
+
+    // Create client address
+    struct sockaddr_in client_addr;
+    int client_addrlen = sizeof(client_addr);
+
+    // Get client address
+    int sockn = getsockname(newsockfd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addrlen);
+    if (sockn < 0)
+    {
+        perror("webserver (getsockname)");
+        close(newsockfd);
+        return NULL;
+    }
+
+    char buffer[1024] = {0};
+
+    // TODO : check difference between read and recv
+
+    // Read from the socket
+    ssize_t valread = recv(newsockfd, buffer, BUFFER_SIZE, 0);
+    if (valread < 0)
+    {
+        perror("webserver (read)");
+        close(newsockfd);
+        return NULL;
+    }
+
+    printf("buffer: %s\n", buffer);
+
+    // TODO : create router func
+
+    // Read the request
+    char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
+    sscanf(buffer, "%s %s %s", method, uri, version);
+
+    printf("[%s:%u] method : '%s' version : '%s' uri : '%s'\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), method, version, uri);
+
+    // compare method with GET
+    if (strcmp(method, "GET") != 0)
+    {
+        printf("method not supported\n");
+        close(newsockfd);
+        return NULL;
+    }
+
+    // compare uri, there are two routes
+    // - /paths
+    // - /files
+
+    // check that beggining starts with /path or /file
+    if (strncmp(uri, "/paths", 5) == 0)
+    {
+        paths(newsockfd, uri);
+        close(newsockfd);
+        return NULL;
+    }
+    else if (strncmp(uri, "/files", 5) != 0)
+    {
+        close(newsockfd);
+        return NULL;
+    }
+    else
+    {
+        printf("uri not supported\n");
+        close(newsockfd);
+        return NULL;
+    }
+}
+
+// localhost:8080/paths?path=./dir&regex=.*
+void paths(int client_socket, const char *uri)
+{
+
+    // extract from uri as /paths?path=dir
+    // the var dir in order to search
+
+    char *dir = extract_path_parameter(uri, "dir");
+    char *regex = extract_path_parameter(uri, "regex");
+
+    searchFiles(dir, regex, client_socket);
+}
+
+void files(int client_socket, const char *uri)
+{
+    // extract from uri as /files?path=dir
+    // the var dir in order to search
+
+    char *dir = extract_path_parameter(uri, "dir");
+    char *regex = extract_path_parameter(uri, "regex");
+
+    // sendFiles(dir, regex, client_socket);
+}
+
+// TODO: first search all the files and then send them
+void searchFiles(const char *dir, const char *regex, int clientSocket)
+{
+
+    DIR *dirp;
+    struct dirent *entry;
+    regex_t reg;
+
+    char path[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+    int fd, bytesRead;
+
+    if (regcomp(&reg, regex, REG_EXTENDED) != 0)
+    {
+        perror("Error compilando el regex");
+        return;
+    }
+
+    if ((dirp = opendir(dir)) == NULL)
+    {
+        perror("Error abriendo el directorio");
+        return;
+    }
+
+    while ((entry = readdir(dirp)) != NULL)
+    {
+        if (regexec(&reg, entry->d_name, 0, NULL, 0) == 0)
+        {
+            sprintf(path, "%s/%s", dir, entry->d_name);
+
+            // Enviar el nombre del archivo encontrado
+            send(clientSocket, path, strlen(path), 0);
+            send(clientSocket, "\n", 1, 0); // Salto de línea
+
+            if ((fd = open(path, O_RDONLY)) == -1)
+            {
+                fprintf(stderr, "Error al abrir el archivo %s: %s\n", path, strerror(errno));
+                continue;
+            }
+
+            // Leer y enviar el archivo
+            while ((bytesRead = read(fd, buffer, BUFFER_SIZE)) > 0)
+            {
+                if (send(clientSocket, buffer, bytesRead, 0) == -1)
+                {
+                    perror("Error enviando el archivo");
+                    close(fd);
+                    return;
+                }
+            }
+
+            if (bytesRead == -1)
+            {
+                fprintf(stderr, "Error leyendo el archivo %s: %s\n", path, strerror(errno));
+            }
+
+            close(fd);
+            printf("Archivo enviado: %s\n", path);
+        }
+    }
+
+    regfree(&reg);
+    closedir(dirp);
+}
+
+void send_file(int client_socket, char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        perror("error opening file");
+        return;
+    }
+
+    // HTTP response with body content
+
+    char response[BUFFER_SIZE];
+
+    size_t bytes_read;
+    int i = 0;
+
+    const char *header = "HTTP/1.0 200 OK\r\n"
+                         "Server: webserver-c\r\n"
+                         "Content-Type: text/plain\r\n"
+                         "Content-Length: %ld\r\n\r\n";
+
+    int body_size = BUFFER_SIZE - strlen(header) - 1;
+    char body_content[body_size];
+
+    while ((bytes_read = fread(body_content, 1, body_size, file)) > 0)
+    {
+
+        snprintf(response, sizeof(response),
+                 "HTTP/1.0 200 OK\r\n"
+                 "Server: webserver-c\r\n"
+                 "Content-Type: text/plain\r\n"
+                 "Content-Length: %ld\r\n\r\n" // Include Content-Length
+                 "%s",                         // Body content
+                 strlen(body_content), body_content);
+
+        printf("response: \n----------------\n%s\n----------------\n", response);
+
+        send(client_socket, response, BUFFER_SIZE, 0);
+
+        memset(body_content, 0, sizeof(body_content));
+        memset(response, 0, sizeof(response));
+
+        printf("\nsent iteration number '%d'\n", i++);
+    }
+
+    fclose(file);
+}
+
+char *extract_path_parameter(const char *uri, char *parameterName)
+{
+
+    // add = to the end of the parameter name
+    char pName[BUFFER_SIZE];
+    sprintf(pName, "%s=", parameterName);
+    char *parameter = malloc(BUFFER_SIZE);
+
+    // Look for "{parameter}=" in the URI
+
+    char *start = strstr(uri, pName);
+    if (start)
+    {
+        // Move pointer to the beginning of the parameterName value
+        start += strlen(pName);
+
+        // Copy the filename value until the end of the string or until '&' (if there are multiple parameters)
+        int i = 0;
+        while (start[i] != '\0' && start[i] != '&' && i < BUFFER_SIZE - 1)
+        {
+            parameter[i] = start[i];
+            i++;
+        }
+        parameter[i] = '\0'; // Null-terminate the parameterName string
+    }
+    else
+    {
+        // If "parameterName=" is not found, return an empty string
+        parameter[0] = '\0';
+    }
+    return parameter;
+}
+
+// gcc server.c -o server -pthread
 
 // free port
 // sudo lsof -i :8080
